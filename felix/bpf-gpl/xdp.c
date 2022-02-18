1178bac7e09f079770b80268ba4ec600c6bfc328
song jiang <song@tigera.io>
2022-02-18 14:10:17 +0000

    fix

--- felix/bpf-gpl/xdp.c
+++ felix/bpf-gpl/xdp.c
@@ -47,11 +47,10 @@ update_trace_entry(__u8 flags, struct cali_tc_ctx *ctx)
 	// update flags
 	state_on_stack.flags = flags;
 
-    // if key_ip is 0, use key_flags
-	//if (!key_ip) {
-		//bpf_map_update_elem(&trace_map, &key_flags, &state_on_stack, 0);
-		//return;
-	//}
+    // if key_ip is 0, do nothing
+	if (!key_ip) {
+		return;
+	}
 
 	// use key_ip
     bpf_map_update_elem(&trace_map, &key_ip, &state_on_stack, 0);
