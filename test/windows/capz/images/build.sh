6700fb5f2018dcefc5de1c0b16e532812f1d6bef
song jiang <song@tigera.io>
2022-05-13 16:02:25 +0000

    Update ebpf-win install

--- test/windows/capz/images/build.sh
+++
@@ -1,4 +0,0 @@
-repository=${repository:-"songtjiang"}
-version=${version:-"01"}
-
-docker buildx build --platform windows/amd64 --output=type=registry --pull --build-arg=BUILD_VERSION=$version -f Dockerfile.install -t $repository/ebpfwin-install:$version .
